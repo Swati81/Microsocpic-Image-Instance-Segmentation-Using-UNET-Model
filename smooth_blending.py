672ef634ccaf0b51053ab357c5dcc202b8de1920
Swati Sinha <52413661+Swati81@users.noreply.github.com>
2022-07-21 00:37:07 +0530

    Update smooth_blending.py

--- smooth_blending.py
+++ smooth_blending.py
@@ -8,59 +8,11 @@ from matplotlib import pyplot as plt
 from skimage import color
 
 
-def satellite_mask_color_restoration(mask):
-    mask = np.array(mask, dtype='uint8')
-    mask = np.dstack((mask, mask, mask))
-    mask[:, :, 0][mask[:, :, 0] == 1] = 63
-    mask[:, :, 0][mask[:, :, 0] == 2] = 117
-    mask[:, :, 0][mask[:, :, 0] == 3] = 110
-    mask[:, :, 0][mask[:, :, 0] == 4] = 18
-    mask[:, :, 0][mask[:, :, 0] == 5] = 226
-    mask[:, :, 0][mask[:, :, 0] == 0] = 155
-
-    mask[:, :, 1][mask[:, :, 1] == 1] = 60
-    mask[:, :, 1][mask[:, :, 1] == 2] = 17
-    mask[:, :, 1][mask[:, :, 1] == 3] = 193
-    mask[:, :, 1][mask[:, :, 1] == 4] = 128
-    mask[:, :, 1][mask[:, :, 1] == 5] = 169
-    mask[:, :, 1][mask[:, :, 1] == 0] = 155
-
-    mask[:, :, 2][mask[:, :, 2] == 1] = 232
-    mask[:, :, 2][mask[:, :, 2] == 2] = 8
-    mask[:, :, 2][mask[:, :, 2] == 3] = 228
-    mask[:, :, 2][mask[:, :, 2] == 4] = 40
-    mask[:, :, 2][mask[:, :, 2] == 5] = 41
-    mask[:, :, 2][mask[:, :, 2] == 0] = 155
-
-    return mask
-
-
 def neucleus_color_mask(mask, col=(155, 40, 52)):
     mask[:, :, 0][mask[:, :, 0] == 1] = col[0]
     mask[:, :, 1][mask[:, :, 1] == 1] = col[1]
     mask[:, :, 2][mask[:, :, 2] == 1] = col[2]
     return  mask
-
-
-def Final_prediction(img, model, patch_size, n_classes):
-    img = img[:, :, ::-1]/255.0
-    input_img = (img.reshape(-1, img.shape[-1])).reshape(img.shape)
-    predictions_smooth = smooth_windowing(
-        input_img,
-        window_size=patch_size,
-        subdivisions=2,
-        nb_classes=n_classes,
-        pred_func=(
-            lambda img_batch_subdiv: model.predict((img_batch_subdiv))
-        )
-    )
-
-    final_prediction = np.argmax(predictions_smooth, axis=2)
-    col_mask = satellite_mask_color_restoration(final_prediction)
-    plt.imsave('output/out.png', col_mask)
-    plt.imsave('output/out.jpg', img)
-    plt.imshow(col_mask)
-    plt.show()
 
 
 class Binary_mask_instance_segmentation:
